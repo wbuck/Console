842345e024fcad095e42351477d45fb825e68c23
Warren Buckley <wbuckley15@hotmail.com>
2016-09-07 07:02:03 -0400

    Add control panel view

---
+++ Console.UI/Converters/BrushToColorConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+
+namespace Console.UI.Converters
+{
+    [ValueConversion( typeof( SolidColorBrush ), typeof( Color ) )]
+    public class BrushToColorConverter : IValueConverter
+    {
+        public object Convert( object value, Type targetType, object parameter, CultureInfo culture )
+        {
+            if( value == null )
+                return null;
+
+            var brush = value as SolidColorBrush;
+
+            if( brush != null )
+                return brush.Color;
+            
+            throw new InvalidOperationException( "Unsupported type [" + value.GetType( ).Name +
+                                                 "], BrushToColorConverter.Convert()" );
+        }
+
+
+        public object ConvertBack( object value, Type targetType, object parameter, CultureInfo culture )
+        {
+            throw new InvalidOperationException( "Unsupported type [" + value.GetType( ).Name +
+                                                 "], ConvertBack.Convert()" );
+        }
+    }
+}
