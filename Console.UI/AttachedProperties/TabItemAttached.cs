54b70bc2e89872243d4e993b06bf802f3ac30959
Warren Buckley <wbuckley15@hotmail.com>
2016-10-10 13:44:58 -0400

    Add attached property to use tab item style

---
+++ Console.UI/AttachedProperties/TabItemAttached.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Console.UI.AttachedProperties
+{
+    public class TabItemAttached
+    {
+        public static readonly DependencyProperty DataProperty =
+            DependencyProperty.RegisterAttached( "Data",
+                typeof( string ),
+                typeof( TabItemAttached ),
+                new FrameworkPropertyMetadata( string.Empty, FrameworkPropertyMetadataOptions.AffectsRender ), ValidateData );
+
+        public static readonly DependencyProperty TextProperty =
+           DependencyProperty.RegisterAttached( "Text",
+               typeof( string ),
+               typeof( TabItemAttached ),
+               new PropertyMetadata( string.Empty ) );
+
+        private static bool ValidateData( object value )
+        {
+            try
+            {
+                Geometry.Parse( ( string )value );
+                return true;
+            }
+            catch( FormatException )
+            {
+                return false;
+            }
+        }
+
+        public static string GetText( DependencyObject obj )
+        {
+            return ( string )obj.GetValue( TextProperty );
+        }
+
+        public static void SetText( DependencyObject obj, string text )
+        {
+            obj.SetValue( TextProperty, text );
+        }
+
+        public static string GetData( DependencyObject obj )
+        {
+            return ( string )obj.GetValue( DataProperty );
+        }
+
+        public static void SetData( DependencyObject obj, string data )
+        {
+            obj.SetValue( DataProperty, data );
+        }
+    }
+}
